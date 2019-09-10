2facf74e8336f72ff6b9a35920ec3f7ce6ec5104
Jodiac92 <fracasado5243@gmail.com>
2019-09-10 09:05:13 +0900

    2019-09-10

---
+++ src/CalendarDemo.java
@@ -0,0 +1,11 @@
+
+import Java_20190723.Calendar;
+public class CalendarDemo {
+	
+	public static void main(String[] args) {
+		java.util.Date d = new java.util.Date();
+		Calendar c = new Calendar();
+		
+		c.print(2019,7,29);
+	}
+}
